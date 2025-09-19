9064d5c032204be3436308ed57d392cdc9113db7
Sourabh George <sourabhgeorgeofficial@gmail.com>
2025-09-19 22:59:46 +0530

    Update EmailService.java

--- src/main/java/com/sourabh/portfolio/portfolio_backend/service/EmailService.java
+++ src/main/java/com/sourabh/portfolio/portfolio_backend/service/EmailService.java
@@ -15,21 +15,26 @@ public class EmailService {
     @Value("${spring.sendgrid.from-email}")
     private String fromEmail;
 
-    public void sendEmail(String to, String subject, String body) throws IOException {
+  public void sendEmail(String to, String subject, String body) {
+    try {
         Email from = new Email(fromEmail);
         Email recipient = new Email(to);
-
         Content content = new Content("text/plain", body);
         Mail mail = new Mail(from, subject, recipient, content);
 
         SendGrid sg = new SendGrid(sendGridApiKey);
         Request request = new Request();
-
         request.setMethod(Method.POST);
         request.setEndpoint("mail/send");
         request.setBody(mail.build());
 
         Response response = sg.api(request);
-        System.out.println("Email sent with status: " + response.getStatusCode());
+        System.out.println("Email status: " + response.getStatusCode());
+        System.out.println("Response body: " + response.getBody());
+        System.out.println("Response headers: " + response.getHeaders());
+    } catch (IOException ex) {
+        ex.printStackTrace();  // <--- log full exception
     }
+}
+
 }
