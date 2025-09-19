6e301fca80f59a94a29cd8db7ee020939892ce6b
Sourabh George <sourabhgeorgeofficial@gmail.com>
2025-09-19 23:14:06 +0530

    Update MessageController.java

--- src/main/java/com/sourabh/portfolio/portfolio_backend/controller/MessageController.java
+++ src/main/java/com/sourabh/portfolio/portfolio_backend/controller/MessageController.java
@@ -1,22 +1,34 @@
 package com.sourabh.portfolio.portfolio_backend.controller;
 
-import com.sourabh.portfolio.portfolio_backend.model.message;
-import com.sourabh.portfolio.portfolio_backend.repository.messagerepository;
+import com.sourabh.portfolio.portfolio_backend.model.Message;
+import com.sourabh.portfolio.portfolio_backend.repository.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = {"http://127.0.0.1:5500", "*"}) // allow only your frontend origin
-
-// Allow frontend to connect (important)
+@CrossOrigin(origins = {"http://127.0.0.1:5500", "*"}) 
 @RequestMapping("/api/messages")
 public class MessageController {
 
+    private final MessageRepository messageRepository;
+
     @Autowired
-    private messagerepository messageRepository;
+    public MessageController(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
 
     @PostMapping
-    public message saveMessage(@RequestBody message message) {
-        return messageRepository.save(message);
+    public ResponseEntity<Message> saveMessage(@RequestBody Message message) {
+        if (message.getName() == null || message.getEmail() == null || message.getContent() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        Message savedMessage = messageRepository.save(message);
+        return ResponseEntity.ok(savedMessage);
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<Message>> getAllMessages() {
+        return ResponseEntity.ok(messageRepository.findAll());
     }
 }
