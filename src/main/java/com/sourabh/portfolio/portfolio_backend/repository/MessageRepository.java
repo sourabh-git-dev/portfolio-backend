587ecfa523b3093b41a6dd52d09cd43fe81881e7
Sourabh George <sourabhgeorgeofficial@gmail.com>
2025-09-20 13:22:03 +0530

    Update MessageRepository.java

--- src/main/java/com/sourabh/portfolio/portfolio_backend/repository/MessageRepository.java
+++ src/main/java/com/sourabh/portfolio/portfolio_backend/repository/MessageRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface MessageRepository extends CrudRepository<Message, Long> {}
-
 public interface MessageRepository extends JpaRepository<Message, Long> {
 }
