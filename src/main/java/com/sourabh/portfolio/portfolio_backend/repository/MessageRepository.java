a6bbf2a963145505ad1352bb14e1a9bb9345a67e
Sourabh George <sourabhgeorgeofficial@gmail.com>
2025-09-20 13:19:18 +0530

    Update and rename messagerepository.java to MessageRepository.java

--- src/main/java/com/sourabh/portfolio/portfolio_backend/repository/MessageRepository.java
+++ src/main/java/com/sourabh/portfolio/portfolio_backend/repository/MessageRepository.java
@@ -1,11 +1,11 @@
 package com.sourabh.portfolio.portfolio_backend.repository;
 
-import com.sourabh.portfolio.portfolio_backend.model.message;
+import com.sourabh.portfolio.portfolio_backend.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface MessageRepository extends CrudRepository<Message, Long> {}
 
-public interface messagerepository extends JpaRepository<message, Long> {
+public interface MessageRepository extends JpaRepository<Message, Long> {
 }
