49f33106b21f136e53dee9450b326f2aab5b7846
Sourabh George <sourabhgeorgeofficial@gmail.com>
2025-09-20 14:26:11 +0530

    Update OtpService.java

--- src/main/java/com/sourabh/portfolio/portfolio_backend/service/OtpService.java
+++ src/main/java/com/sourabh/portfolio/portfolio_backend/service/OtpService.java
@@ -20,23 +20,25 @@ public class OtpService {
 
     // Generate OTP and send email via SendGrid
     public void generateAndSendOtp(String email) {
-        String otpCode = String.valueOf(100000 + new Random().nextInt(900000));
+    String otpCode = String.valueOf(100000 + new Random().nextInt(900000));
 
-        // Remove old OTPs for the same email
-        otpRepository.findByEmailAndCode(email, otpCode)
-                .ifPresent(otpRepository::delete);
+    // Remove old OTPs for the same email
+    otpRepository.findByEmailAndCode(email, otpCode)
+            .ifPresent(otpRepository::delete);
 
-        Otp otp = new Otp();
-        otp.setEmail(email);
-        otp.setCode(otpCode);
-        otp.setExpiryTime(LocalDateTime.now().plusMinutes(5));
+    Otp otp = new Otp();
+    otp.setEmail(email);
+    otp.setCode(otpCode);
+    otp.setExpiryTime(LocalDateTime.now().plusMinutes(5));
 
-        otpRepository.save(otp);
+    otpRepository.save(otp);
 
-     
-            emailService.sendEmail(email, "Your OTP Code", "Your OTP is: " + otpCode + "\n(valid for 5 minutes).");
-        
-    }
+    String subject = "Portfolio App: Your OTP Code";
+    String body = "Hello,\n\nYour OTP is: " + otpCode + 
+                  "\nIt is valid for 5 minutes only.\n\nPortfolio App";
+    emailService.sendEmail(email, subject, body, "Portfolio App <portfoliosourabh@gmail.com>");
+}
+
 
     // Verify OTP
     public boolean verifyOtp(String email, String code) {
